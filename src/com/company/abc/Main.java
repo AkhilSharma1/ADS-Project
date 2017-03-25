efadea0b0e669dc74c04b2445b329fa2517fa39e
Akhil <coolakhil.123@gmail.com>
2017-03-26 02:49:47 +0530

    init project

---
+++ src/com/company/abc/Main.java
@@ -0,0 +1,12 @@
+package com.company.abc;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+//        generateFreqTableFromUserInput()
+
+    }
+}
