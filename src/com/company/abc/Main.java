fa2b50697e07848bf36e42e2651c5bf35bb7ff17
Akhil <coolakhil.123@gmail.com>
2017-04-01 22:19:35 +0530

    add code for FourWay Heap

--- src/com/company/abc/Main.java
+++ src/com/company/abc/Main.java
@@ -5,7 +5,6 @@ import com.company.abc.heaps.PQ_ENUM;
 import com.company.abc.utils.CoreUtils;
 import com.company.abc.utils.FileUtils;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,16 +15,17 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> inputData = FileUtils.readInputFile(args[0]);
-        System.out.println("Input is :");
-        System.out.println(inputData);
+//        System.out.println("Input is :");
+//        System.out.println(inputData);
         frequencyTable = CoreUtils.generateFreqTableFromInput(inputData);
-        System.out.println("frequencyTable is:");
+//        System.out.println("frequencyTable is:");
 
-        System.out.printf(frequencyTable+"\n");
+//        System.out.printf(frequencyTable+"\n");
 
-        //PQ_ENUM fastestPQ = comparePriorityQueues(frequencyTable);
+        PQ_ENUM fastestPQ = comparePriorityQueues(frequencyTable);
+        System.out.println(fastestPQ);
 //        PQ_ENUM fastestPQ = PQ_ENUM.PAIRING_HEAP;
-        PQ_ENUM fastestPQ = PQ_ENUM.BINARY_HEAP;
+       /* PQ_ENUM fastestPQ = PQ_ENUM.BINARY_HEAP;
 
         HuffmanTree huffmanTree = new HuffmanTree(fastestPQ, frequencyTable);
       //build code Table
@@ -47,6 +47,16 @@ public class Main {
 
 
 
+        System.out.println("4WAY HEAP code table: ==================");
+        HuffmanTree huffmanTree2 = new HuffmanTree(PQ_ENUM.FOUR_WAY_HEAP, frequencyTable);
+
+        //build code Table
+        HashMap<Integer, String> codeTable2 = new HashMap<>();
+        huffmanTree2.buildCodeTableFromHuffmanTree(codeTable2);
+        System.out.printf("");
+        System.out.println(codeTable2);*/
+
+
     }
 
     public static PQ_ENUM comparePriorityQueues(Map<Integer, Integer> frequencyTable) {
@@ -54,11 +64,13 @@ public class Main {
         long startTime, endTime, timeTakenBH, timeTaken4WH, timeTakenPH;
 
         startTime = System.currentTimeMillis();
-        for(int i = 0; i<5; i++){
+        for(int i = 0; i<1; i++){
             new HuffmanTree(PQ_ENUM.BINARY_HEAP, frequencyTable);
         }
         endTime = System.currentTimeMillis();
         timeTakenBH = endTime - startTime;
+        System.out.println("Time in ms taken BH: " + timeTakenBH);
+
 
         startTime = System.currentTimeMillis();
         for(int i = 0; i<5; i++){
@@ -66,6 +78,8 @@ public class Main {
         }
         endTime = System.currentTimeMillis();
         timeTaken4WH = endTime - startTime;
+        System.out.println("Time in ms taken 4WH: " + timeTaken4WH);
+
 
         startTime = System.currentTimeMillis();
         for(int i = 0; i<5; i++){
@@ -73,6 +87,7 @@ public class Main {
         }
         endTime = System.currentTimeMillis();
         timeTakenPH = endTime - startTime;
+        System.out.println("Time in ms taken PH: " + timeTakenPH);
 
 
         if(timeTakenBH<timeTaken4WH && timeTakenBH<timeTakenPH)
--- src/com/company/abc/heaps/FourWayHeap.java
+++ src/com/company/abc/heaps/FourWayHeap.java
@@ -1,32 +1,93 @@
 package com.company.abc.heaps;
 
 
+import java.util.Arrays;
+
 /**
  * Created by hfire on 3/30/17.
  */
-public class FourWayHeap<T extends Comparable> implements PriorityQueue {
+public class FourWayHeap implements PriorityQueue {
+
+    private Node[] array = new Node[10];
+    private int size;            // keeps a count of number of elements in the heap
+    private int numWays = 4;            // 4-way heap, number of children a each node has
+
     @Override
     public void add(Node newNode) {
+        if (size >= array.length - 1) {
+            array = this.resize();
+        }
+        array[size] = newNode;
+        heapUp(size);
+        size++;
+    }
 
+    public void heapUp(int i) {
+        Node child = array[i];
+        int parentIndex = (i-1)/numWays;
+        Node parent = array[parentIndex];
+        while ((i > 0) && (child.compareTo(parent) < 0)) {
+            array[i] = array[parentIndex];
+            i = parentIndex;
+            parentIndex = (parentIndex-1)/numWays;
+        }
+        array[i] = child;
     }
 
     @Override
     public Node removeMin() {
-        return null;
+
+        Node min = array[0];
+        size--;
+        // set the last element as root
+        array[0] = array[size];
+        heapDown(0);
+        return min;
     }
+
+    // heapDown() method, this method compares the replacement element with the max of children,
+    // if the largest child is larger than this replacement element, we move the child up, we continue doing this until heap order is correct and put the replacement element in correct index.
+    public void heapDown(int ix) {
+        int minChild = findMin(ix*numWays+1,ix*numWays+numWays);
+       Node tempRoot = array[ix];       // save root in temp variable
+        while ((minChild < size) && (array[minChild].compareTo( tempRoot)<0)){
+            array[ix] = array[minChild];
+            ix = minChild;
+            minChild = findMin(minChild*numWays+1, minChild*numWays+numWays);
+        }
+        array[ix] = tempRoot;
+    }
+
+    // find minimum of the children
+    private int findMin(int from, int to) {
+        int minChild = from;
+        for (int i = from+1; (i<=to && i< size); i++){
+            if ((array[minChild].compareTo (array[i])>0))
+                minChild = i;
+        }
+        return minChild;
+    }
+
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Node peek() {
         return null;
     }
+
+
+    protected Node[] resize() {
+        return Arrays.copyOf(array, array.length * 2);
+    }
+
+
 }
